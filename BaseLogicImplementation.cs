9f168581b95fa721333ef6174d98b37f3d03ff72
VasilSarandev <WSarandev@gmail.com>
2015-02-16 20:00:11 +0200

    Added crash tests, AI implementation for bots, Target and logic for acquiring target by player.

--- BaseLogicImplementation.cs
+++ BaseLogicImplementation.cs
@@ -39,11 +39,12 @@ namespace TankWars
                 "*",
                 "#"
             };
+            int score = 0;
             int matrixWidth = 20;
             int matrixHeight = 20;
             int botsCount = 4;
             Console.BufferHeight = Console.WindowHeight;
-            Position Player = new Position(0, 0,0);
+            Position Player = new Position(0, 0, 0);
             Console.SetCursorPosition(Player.X, Player.Y);
             Console.Write("(0)");
             Random RandomIntGenerator = new Random();
@@ -51,18 +52,23 @@ namespace TankWars
             //generate bots.
             for (int i = 0; i < botsCount; i++)
             {
-                Bots.Add(new Position(RandomIntGenerator.Next(0, matrixWidth), RandomIntGenerator.Next(0, matrixHeight),RandomIntGenerator.Next(0,3)));
+                Bots.Add(new Position(RandomIntGenerator.Next(3, matrixWidth), RandomIntGenerator.Next(3, matrixHeight), RandomIntGenerator.Next(0, 3)));
             }
+            //generate target
+            Position Target = new Position(RandomIntGenerator.Next(5, matrixWidth - 5), RandomIntGenerator.Next(4, matrixHeight - 4), 0);
+            bool targetAcquired = false;
             // draw bots
-            
-           
+
+
             for (int i = 0; i < Bots.Count; i++)
             {
                 Console.SetCursorPosition(Bots[i].X, Bots[i].Y);
                 Console.Write(bots[i]);
             }
             //game loop logic.
-            while (true)
+            bool gamerunning = true;
+
+            while (gamerunning)
             {
                 //move player
                 if (Console.KeyAvailable)
@@ -71,7 +77,7 @@ namespace TankWars
                     if (userInput.Key == ConsoleKey.RightArrow)
                     {
                         Direction = 0;
-                        
+
                     }
                     else if (userInput.Key == ConsoleKey.LeftArrow)
                     {
@@ -86,33 +92,120 @@ namespace TankWars
                     {
                         Direction = 3;
                     }
-                    Player = new Position(Player.X + Directions[Direction].X, Player.Y - Directions[Direction].Y,0);
+                    Player = new Position(Player.X + Directions[Direction].X, Player.Y - Directions[Direction].Y, 0);
                 }
                 //test
-                
-                int randomDirection=0;
+
+                int randomDirection = 0;
+                //crash tests
+
+                int crashedBotCoordinateX = 0;
+                int crashedBotCoordinateY = 0;
+                int crashedBotCurrentDirection = 0;
+                for (int bot = 0; bot < botsCount; bot++)
+                {
+                    if (Bots[bot].X <= 3
+
+                        )
+                    {
+                        crashedBotCoordinateX = Bots[bot].X;
+                        crashedBotCoordinateY = Bots[bot].Y;
+                        crashedBotCurrentDirection = 0;
+                        Bots.RemoveAt(bot);
+                        Bots.Add(new Position(crashedBotCoordinateX, crashedBotCoordinateY, crashedBotCurrentDirection));
+
+                    }
+                    if (Bots[bot].X > matrixWidth - 3)
+                    {
+                        crashedBotCoordinateX = Bots[bot].X;
+                        crashedBotCoordinateY = Bots[bot].Y;
+                        crashedBotCurrentDirection = 1;
+                        Bots.RemoveAt(bot);
+                        Bots.Add(new Position(crashedBotCoordinateX, crashedBotCoordinateY, crashedBotCurrentDirection));
+                    }
+                    if (Bots[bot].Y < 3)
+                    {
+                        crashedBotCoordinateX = Bots[bot].X;
+                        crashedBotCoordinateY = Bots[bot].Y;
+                        crashedBotCurrentDirection = 3;
+                        Bots.RemoveAt(bot);
+                        Bots.Add(new Position(crashedBotCoordinateX, crashedBotCoordinateY, crashedBotCurrentDirection));
+                    }
+                    if (Bots[bot].Y > matrixHeight - 3)
+                    {
+                        crashedBotCoordinateX = Bots[bot].X;
+                        crashedBotCoordinateY = Bots[bot].Y;
+                        crashedBotCurrentDirection = 2;
+                        Bots.RemoveAt(bot);
+                        Bots.Add(new Position(crashedBotCoordinateX, crashedBotCoordinateY, crashedBotCurrentDirection));
+                    }
+                }
+                //check if dead.
+
+
+
                 //move bots.
                 List<Position> CopyBots = new List<Position>(Bots);
                 Bots.Clear();
                 for (int i = 0; i < botsCount; i++)
                 {
-                    randomDirection=CopyBots[i].CurrentDirectionBot;
+                    randomDirection = CopyBots[i].CurrentDirectionBot;
                     Bots.Add(new Position(CopyBots[i].X + Directions[randomDirection].X, CopyBots[i].Y - Directions[randomDirection].Y, randomDirection));
                 }
-                    
+
+                //check if target is acquired.
+                if (Player.X == Target.X && Player.Y == Target.Y)
+                {
+                    targetAcquired = true;
+
+                }
+                if (targetAcquired)
+                {
+                    Target = new Position(RandomIntGenerator.Next(5, matrixWidth - 5), RandomIntGenerator.Next(4, matrixHeight - 4), 0);
+                    targetAcquired = false;
+                    score++;
+                }
+
+
 
                 //draw stuff
-                
+
                 Console.Clear();
+                //player
                 Console.SetCursorPosition(Player.X, Player.Y);
                 Console.Write("(0)");
+                //score
+                Console.SetCursorPosition(50, 10);
+                Console.Write("Your score is ... : {0}", score);
+                //target
+                Console.SetCursorPosition(Target.X, Target.Y);
+                Console.Write("T");
+
+
+                //bots
                 for (int i = 0; i < botsCount; i++)
                 {
                     Console.SetCursorPosition(Bots[i].X, Bots[i].Y);
                     Console.Write(bots[i]);
                 }
-                Thread.Sleep(700);
-                 
+                Thread.Sleep(200);
+                //check if dead.
+                for (int i = 0; i < Bots.Count; i++)
+                {
+                    if (Bots[i].X == Player.X && Bots[i].Y == Player.Y)
+                    {
+                        Console.Clear();
+                        Console.SetCursorPosition(0, 0);
+                        Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write("Game OVER ! Your score is ... {0} ", score);
+                        gamerunning = false;
+
+
+
+                    }
+                }
+
             }
 
 
